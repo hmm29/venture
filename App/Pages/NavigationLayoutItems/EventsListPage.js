dc8c432db8d1938f7abaaf970ac0e3e33dc96d8e
Harrison Miller <harrisonmiller@Harrisons-MacBook-Pro.local>
2016-02-08 21:57:17 -0500

    Add genders.json and image slider component, put copyright comment on top of all component files

--- App/Pages/NavigationLayoutItems/EventsListPage.js
+++ App/Pages/NavigationLayoutItems/EventsListPage.js
@@ -1,3 +1,11 @@
 /**
- * Created by harrisonmiller on 2/8/16.
+ * Copyright (c) 2015-present, Venture Applications, LLC.
+ * All rights reserved.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Harrison Miller <hmaxmiller@gmail.com>, September 2015
+ *
+ * @providesModule EventsListPage
+ * @flow
  */
